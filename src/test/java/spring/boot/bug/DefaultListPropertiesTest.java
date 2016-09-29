8dddd0f68ea2e691e154d1e625900ae210d891e0
AlexanderPruss <alexander.pruss@kreuzwerker.de>
2016-09-29 11:51:16 +0200

    Added another test condition.

--- src/test/java/spring/boot/bug/DefaultListPropertiesTest.java
+++ src/test/java/spring/boot/bug/DefaultListPropertiesTest.java
@@ -17,13 +17,14 @@ public class DefaultListPropertiesTest {
 
 	/**
 	 * BUG:
-	 * 1. The configured list in application.yml is a list with a single element, "bar".
+	 * 1. The configured list in application.yml is a list with a single element, {"bar"}.
 	 * 2. The default value of the list in DefaultListProperties is a list with two elements : {"foo", "foo"}
 	 * 3. Instead of replacing the default value, the configured list is put into the beginning of the list.
 	 * Result: {"bar", "foo"}.
 	 */
 	@Test
 	public void listEqualsConfiguredList() {
+		assertEquals("bar", properties.getConfiguredList().get(0));
 		assertEquals(1, properties.getConfiguredList());
 	}
 
